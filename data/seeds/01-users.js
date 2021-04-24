b3ffc2326724036b44ee8501b61993f80e4cbdb0
Alan Mir <alanpaulphotos@gmail.com>
2021-04-23 22:20:57 -0400

    Added more data to users seed

---
+++ data/seeds/01-users.js
@@ -0,0 +1,44 @@
+exports.seed = function(knex) {
+  return knex('clients').insert([
+    {
+      user_id: 1,
+      user_name: "Marcus Lee",
+      user_username: "TheBest",
+      user_email: "marcuslee@gmail.com",
+      user_password: "mynamemarcus",
+      user_level: "Intermediate",
+      user_subscribed: false,
+      role: 1
+    },
+    {
+      user_id: 2,
+      user_name: "Jack Black",
+      user_username: "Singer",
+      user_email: "tenD@gmail.com",
+      user_password: "jackattack",
+      user_level: "Profesional",
+      user_subscribed: true,
+      role: 1 
+    },
+    {
+      user_id: 3,
+      user_name: "Brad Pitt",
+      user_username: "button",
+      user_email: "brad@aol.com",
+      user_password: "stillluvangie",
+      user_level: "Profesional",
+      user_subscribed: true,
+      role: 2
+    },
+    {
+      user_id: 4,
+      user_name: "Daniel Rosales",
+      user_username: "TheOne",
+      user_email: "dani23@gmail.com",
+      user_password: "allthepower",
+      user_level: "Beginner",
+      user_subscribed: false,
+      role: 1
+    }
+  ]);
+};
