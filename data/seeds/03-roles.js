bbe8e15733654b847563d50d968a7c3d03048c26
Alan Mir <alanpaulphotos@gmail.com>
2021-04-23 22:25:50 -0400

    Fixed typos

--- data/seeds/03-roles.js
+++ data/seeds/03-roles.js
@@ -2,11 +2,11 @@ exports.seed = function(knex) {
   return knex('roles').insert([
     {
       role_id: 1,
-      name: "Instructor"
+      name: "Client"
     },
     {
       role_id: 2,
-      name: "Client"
+      name: "Instructor"
     }
   ]);
 };
