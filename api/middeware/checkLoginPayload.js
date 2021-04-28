091a953bda501e1fd9ddabf6cad83992c9c29c68
Alan Mir <alanpaulphotos@gmail.com>
2021-04-27 22:00:29 -0400

    Fixed typos

--- api/middeware/checkLoginPayload.js
+++ api/middeware/checkLoginPayload.js
@@ -1,9 +1,9 @@
 module.exports = async (req, res, next) => {
     const creds = req.body;
 
-    if(!creds.username || creds.username === '') {
+    if(!creds.user_username || creds.user_username === '') {
         res.status(400).json({message: "Missing username and/or password"});
-    } else if(!creds.password || creds.password === '') {
+    } else if(!creds.user_password || creds.user_password === '') {
         res.status(400).json({message: "Missing username and/or password"});
     } else {
         next();
