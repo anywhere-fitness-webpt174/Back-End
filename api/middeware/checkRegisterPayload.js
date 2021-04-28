6cfbe6e80ccb3b35914f27193907b211d4d3dadd
Alan Mir <alanpaulphotos@gmail.com>
2021-04-27 21:58:42 -0400

    Fixed bugs in register payload middleware

--- api/middeware/checkRegisterPayload.js
+++ api/middeware/checkRegisterPayload.js
@@ -1,13 +1,13 @@
 module.exports = async (req, res, next) => {
     const newClient = req.body;
 
-    if(newClient.name === '' || !newClient.name) {
+    if(newClient.user_name === '' || !newClient.user_name) {
         res.status(400).json({message: "Credentials Incomplete"});
-    } else if(!newClient.username || newClient.username === '') {
+    } else if(!newClient.user_username || newClient.user_username === '') {
         res.status(400).json({message: "Credentials Incomplete"});
-    } else if(!newClient.email || newClient.email === '') {
+    } else if(!newClient.user_email || newClient.user_email === '') {
         res.status(400).json({message: "Credentials Incomplete"});
-    } else if(!newClient.password || newClient.password === '') {
+    } else if(!newClient.user_password || newClient.user_password === '') {
         res.status(400).json({message: "Credentials Incomplete"});
     } else {
         next();
